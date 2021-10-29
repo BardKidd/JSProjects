5f94d25bd6eed029bf85f46ba5568eb6cf7d029a
Lin Pei Wen <mobilesuit0922@gmail.com>
2021-10-29 21:02:59 +0800

    feat: CountdowmTimer > JS 倒數計時器完成

---
+++ CountdownTimer/script.js
@@ -0,0 +1,25 @@
+// 宣告
+let days = document.getElementById("days");
+let hours = document.getElementById("hours");
+let mins = document.getElementById("mins");
+let seconds = document.getElementById("seconds");
+
+let newYears = new Date("2022-01-01 00:00:00").getTime();
+function diff() {
+  const now = new Date().getTime();
+  const diffTime = newYears - now;
+
+  // 取得該數的最大整數
+  const getSeconds = Math.floor((diffTime / 1000) % 60);
+  const getMins = Math.floor((diffTime / 1000 / 60) % 60);
+  const getHours = Math.floor((diffTime / 1000 / 60 / 60) % 60);
+  const getDays = Math.floor(diffTime / 1000 / 60 / 60 / 24);
+
+  days.innerHTML = getDays;
+  hours.innerHTML = getHours;
+  mins.innerHTML = getMins;
+  seconds.innerHTML = getSeconds;
+}
+setInterval(() => {
+  diff();
+}, 1000);
