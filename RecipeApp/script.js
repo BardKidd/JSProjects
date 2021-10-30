54decbecfc6e2027b3a4f17528cc200b87d23468
Lin Pei Wen <mobilesuit0922@gmail.com>
2021-10-30 21:33:40 +0800

    feat: RecipeApp > 準備食材區塊左右滑動完成；圖片從遠端改為本機

--- RecipeApp/script.js
+++ RecipeApp/script.js
@@ -1,11 +1,18 @@
 const foods = document.getElementById("foods");
 
-// foods.addEventListener("scroll", (e) => {
-//   console.log(foods.scrollTop);
-//   console.log(window);
-// });
+let initNum = 1;
+
 foods.addEventListener("wheel", (e) => {
-  foods.scrollLeft += 5;
   e.preventDefault();
-  //   console.log(foods.scrollLeft);
+  let lastNum = initNum;
+
+  // deltaY 往上滾是正數，往下滾是負數
+  initNum += e.deltaY * -0.01;
+
+  // 如
+  if (initNum > lastNum) {
+    foods.scrollLeft -= 10;
+  } else {
+    foods.scrollLeft += 10;
+  }
 });
