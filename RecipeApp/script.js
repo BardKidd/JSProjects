ff6395563ea06129ae0553d0f96d5a4ec396fd1c
Lin Pei Wen <mobilesuit0922@gmail.com>
2021-11-01 22:30:28 +0800

    feat: RecipeApp > 使用 fetch 串接隨機產生餐點 API

--- RecipeApp/script.js
+++ RecipeApp/script.js
@@ -1,7 +1,9 @@
 const foods = document.getElementById("foods");
+const meal = document.getElementById("oneMeal");
+const meal_img = document.getElementById("oneMeal_img");
+const meal_title = document.getElementById("oneMeal_title");
 
 let initNum = 1;
-
 foods.addEventListener("wheel", (e) => {
   e.preventDefault();
   let lastNum = initNum;
@@ -9,10 +11,44 @@ foods.addEventListener("wheel", (e) => {
   // deltaY 往上滾是正數，往下滾是負數
   initNum += e.deltaY * -0.01;
 
-  // 如
   if (initNum > lastNum) {
     foods.scrollLeft -= 10;
   } else {
     foods.scrollLeft += 10;
   }
 });
+
+meal.style.display = "none";
+// 隨機產出餐點資料
+async function getRandomMeal() {
+  const res = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+  const resData = await res.json();
+  const randomMeal = resData.meals[0];
+  meal.style.display = "flex";
+
+  meal_img.src = randomMeal.strMealThumb;
+  console.log(randomMeal);
+}
+
+// 取得餐點 By Id
+async function getMealById(id) {
+  const meal = await fetch(
+    `"https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  );
+}
+
+// 搜尋餐點
+async function getMealsBySearch(keyword) {
+  const meals = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`
+  );
+}
+
+// 取得餐點成分縮圖
+async function getIngredientThumbnail(name) {
+  const Ingredient =
+    await fetch(`www.themealdb.com/images/ingredients/${name}-Small.png
+  `);
+}
+
+getRandomMeal();
