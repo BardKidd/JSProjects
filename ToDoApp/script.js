c585aaf6fa8acaf5b1e4545d4fcd3439cad2e2ac
Lin Pei Wen <mobilesuit0922@gmail.com>
2021-11-06 12:37:21 +0800

    feat: ToDoApp > ToDoApp 資料夾改名；ToDo App 新增 localStorage 儲存以及右鍵刪除功能

---
+++ ToDoApp/script.js
@@ -0,0 +1,69 @@
+const form = document.getElementById("from");
+const input = document.getElementById("input");
+const todos = document.getElementById("todos");
+const savedItem = JSON.parse(localStorage.getItem("todos")) || [];
+
+if (savedItem.length > 0) {
+  savedItem.forEach((todo) => {
+    addTodo(todo);
+    // if (todo.isFinish) {
+    //   todoEl.classList.toggle("isFinish");
+    // }
+    // const todoEl = document.createElement("li");
+    // todoEl.addEventListener("click", () => {
+    //   todoEl.classList.toggle("isFinish");
+    //   setToLocalStorage();
+    // });
+    // if (todo.isFinish) {
+    //   todoEl.classList.toggle("isFinish");
+    // }
+
+    // todoEl.innerText = todo.text;
+    // todos.appendChild(todoEl);
+  });
+}
+
+// 新增項目
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTodo();
+});
+
+function addTodo(todo) {
+  const text = input.value || todo.text;
+
+  if (text) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.isFinish) {
+      todoEl.classList.toggle("isFinish");
+    }
+    todoEl.innerText = text;
+
+    todos.appendChild(todoEl);
+
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("isFinish");
+      setToLocalStorage();
+    });
+    input.value = "";
+    setToLocalStorage();
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      todoEl.remove();
+      setToLocalStorage();
+    });
+  }
+}
+
+function setToLocalStorage() {
+  const liArr = document.querySelectorAll("li");
+
+  let tempArr = [];
+  liArr.forEach((item) => {
+    tempArr.push({
+      text: item.innerText,
+      isFinish: item.classList.contains("isFinish"),
+    });
+  });
+  localStorage.setItem("todos", JSON.stringify(tempArr));
+}
